3e3b2432bc833b0639e238de4a196b09b38e3071
Karthick-dev17 <karthicknagaraj17@gmail.com>
2025-05-09 14:29:13 +0530

    feat:added my file

--- pages/admin/college/index.tsx
+++ pages/admin/college/index.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react';
 import Card, { CardBody } from '../../../components/bootstrap/Card';
 import Button from '../../../components/bootstrap/Button';
 import axios from 'axios';
+import { useRouter } from 'next/router';
+
 
 interface Colleges {
 	id: number;
@@ -18,27 +20,34 @@ async function fetchdata(): Promise<Colleges[]> {
 }
 
 const index = () => {
-
 	const [collegeData, setCollegeData] = useState<Colleges[]>([]);
 
+	const router = useRouter();
+
+	function handleNavigateToCreate() {
+		router.push('/admin/college/create');
+	}
+
+	console.log(collegeData);
+
+	const getData = async () => {
+		try {
+			const data = await fetchdata();
+			setCollegeData(data);
+		} catch (error: any) {
+			// console.log(err);
+
+			console.error('Handled error in useEffect:', {
+				message: error.message,
+				code: error.code,
+				status: error.response?.status,
+			});
+		}
+	};
+
 	useEffect(() => {
-		
-		const getData = async () => {
-			try {
-				const data = await fetchdata();
-				setCollegeData(data);
-			} catch (error: any) {
-				console.error('Handled error in useEffect:', {
-					message: error.message,
-					code: error.code,
-					status: error.response?.status,
-				});
-			}
-		};
-	
 		getData();
 	}, []);
-	
 
 	return (
 		<>
@@ -46,7 +55,12 @@ const index = () => {
 				<div className='d-flex justify-content-between mx-4 mb-1 mt-4'>
 					<h4 className='fw-semibold m-0'>Colleges</h4>
 					<div>
-						<Button icon='ControlPoint' color='success' isLight className=''>
+						<Button
+							icon='ControlPoint'
+							color='success'
+							isLight
+							className=''
+							onClick={handleNavigateToCreate}>
 							Create
 						</Button>
 					</div>
@@ -60,25 +74,32 @@ const index = () => {
 								<th scope='col'>Colleges</th>
 								<th scope='col'>Created by</th>
 								<th scope='col'>Updated by</th>
-								{/* <th scope='col'>Store</th> */}
 								<th scope='col ' className='text-center'>
 									Actions
 								</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope='row'>1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-								{/* <td>Otto</td> */}
-								<td>Otto</td>
-								<td className='d-flex gap-2 justify-content-center'>
-									{' '}
-									<Button color='danger' isLight icon='delete' tag='a' />{' '}
-									<Button color='info' isLight icon='Edit' tag='a' />
-								</td>
-							</tr>
+							{collegeData.length ? (
+								collegeData.map((data, index) => (
+									<tr key={data.id}>
+										<th scope='row'>{index + 1}</th>
+										<td>{data.collegename}</td>
+										<td>{data.createdby}</td>
+										<td>{data.updatedby.join(', ')}</td>
+										<td className='d-flex gap-2 justify-content-center'>
+											<Button color='danger' isLight icon='delete' tag='a' />{' '}
+											<Button color='info' isLight icon='Edit' tag='a' />
+										</td>
+									</tr>
+								))
+							) : (
+								<tr>
+									<td colSpan={5} className='text-center text-muted py-4 fs-5'>
+										No datas
+									</td>
+								</tr>
+							)}
 						</tbody>
 					</table>
 				</CardBody>
